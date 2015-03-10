9a1cce981fcb3145baf0c357d33f97362a3208b7
Sam <samdemorest@gmail.com>
2015-03-09 23:18:41 -0400

    Started Miller-Rabin primality test because AKS so slow. Going with total naive implementation. Not sure if bigint_exp function going to cause problems.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 #![feature(core)]
+#![feature(old_io)]
+#![feature(old_path)]
 extern crate num;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate rand;
 extern crate core;
-use num::bigint::BigUint;
-use num::bigint;
 use num::traits::*;
 use num::integer::Integer;
-use num::bigint::{ToBigUint, RandBigInt};
-use std::fmt;
+use num::bigint::{BigUint, ToBigUint, RandBigInt};
+use std::num::{FromPrimitive, ToPrimitive, Float};
 use std::old_io as io;
 use std::old_io::File;
 use rustc_serialize::base64::*;
-use std::num::FromPrimitive;
-use std::num::ToPrimitive;
-use std::num::Float;
-use std::usize;
+use rand::Rng;
 use core::ops::*;
-
-use num::bigint::BigInt::*;
-use std::{env};
+use std::{env, fmt, usize};
 
 /*
  * Main driver for the program. Lots of experimentation going on here on how to do various tasks
@@ -33,23 +28,73 @@ fn main() {
     } else {
         panic!("Needs an argument for key bit size!");
     }
-    let max_exp: u64 = 
-        bit_size.add(1.to_usize().unwrap()).to_f64().unwrap().log2().ceil().to_u64().unwrap();
-    let mut optional: BigUint;
-    let mut bignum: BigUint;
-    let mut rnjesus = rand::thread_rng();
-    bignum = rnjesus.gen_biguint(bit_size);
-    let mut big_vec: Vec<BigUint> = Vec::new();
-    println!("Big number incoming: {}", bignum);
-
-    let copied: BigUint = bignum.clone();
-    let another: BigUint = BigUint::from_u32(69).unwrap();
-    //write_bnum(bignum.clone());
-    println!("Even? {}", bignum.is_even());
-    println!("Prime? {}", check_primality(bignum.clone(), max_exp));
+    //gen_large_prime(bit_size);
+    bigint_exp(BigUint::from_usize(2).unwrap(), BigUint::from_usize(8).unwrap());
 
 }
 
+fn gen_large_prime(bit_size: usize){
+    let mut bignum: BigUint;
+    let mut rnjesus = rand::OsRng::new().unwrap();
+    let mut is_prime: bool = false;
+    let max_exp: u64 = 
+        bit_size.add(1.to_usize().unwrap()).to_f64().unwrap().log2().ceil().to_u64().unwrap();
+    
+    while is_prime  == false{
+       bignum = rnjesus.gen_biguint(bit_size);
+       if bignum.clone().is_even(){
+           continue;
+       } 
+       /*
+        * 5 should be a sufficient number of runs for the Miller-Rabin test. The greatest
+        *   possibility that the test gives an inaccurate result is 25%. 0.25^5 = 0.0009765, which
+        *   is an acceptable threshold of accuracy.
+        */
+       is_prime = miller_rabin(bignum.clone(), 5);
+       if is_prime{
+           println!("Prime discovered: {}", bignum.clone());
+       }
+    }
+
+}
+
+
+fn miller_rabin(bignum: BigUint, num_runs: usize) -> bool {
+    let mut rnjesus = rand::OsRng::new().unwrap();
+    let mut d: BigUint = bignum.clone().sub(BigUint::one());
+    let mut a: BigUint;
+    let mut x: BigUint;
+    let mut s: usize = 0;
+    while d.is_even() {
+        s += 1;
+        d = d.clone().div(BigUint::from_usize(2).unwrap());
+    }
+    for i in 0..num_runs {
+        a = rnjesus.gen_biguint_range(&BigUint::from_usize(2).unwrap(), 
+                                                   &bignum.clone().sub(BigUint::one()));
+        x = bigint_exp(a.clone(), d.clone());
+
+
+    }
+    return false;
+}
+
+fn mod_exp(d: BigUint, n: BigUint) -> BigUint{
+    let mut retval: BigUint;
+
+    return n;
+}
+
+fn bigint_exp(base: BigUint, pow: BigUint) -> BigUint{
+    let b: BigUint = base.clone();
+    let mut retval: BigUint = base.clone();
+    let mut counter: BigUint = BigUint::one();
+    while counter.clone().lt(&pow.clone()){
+        retval = retval.clone().mul(b.clone());
+        counter = counter.clone().add(BigUint::one());
+    }
+    return retval;
+}
 /**
  * This function writes the bytes of a BigUint to a file.
  */
@@ -74,18 +119,16 @@ fn check_primality(bignum: BigUint, max_exp: u64) -> bool {
 
     // for k, j <= lg(sizeof(bignum))
     // if k^j == bignum return false
-    let mut iterator: BigUint = BigUint::from_usize(2).unwrap();
+    // Start iterator at 3, since if bignum is even, it won't be prime.
+    let mut iterator: BigUint = BigUint::from_usize(3).unwrap();
     let mut r = iterator.clone();
-    if bignum.is_even(){
-        return false;
-    }
+
     while r < bignum{
        if r.gcd(&bignum.clone()) != BigUint::one(){
            return false;
        }
-       r = r.clone().add(BigUint::one());
+       r = r.clone().add(BigUint::from_usize(2).unwrap());
     }
-    println!("{}", max_exp);
     while iterator < bignum{
         if num::pow(iterator.clone(), 2.to_usize().unwrap()).gt(&bignum.clone()){
             return false;
@@ -97,12 +140,7 @@ fn check_primality(bignum: BigUint, max_exp: u64) -> bool {
                 return false;
             }
         }
-        /*
-         * Have to do this temp swap here because iterator is moved when we enter the loop,
-         * and the BigUint type is not copyable.
-         */
-        let temp: BigUint = iterator.clone().add(BigUint::one());
-        iterator = temp;
+        iterator = iterator.clone().add(BigUint::one());
     }
     return false;
 }
@@ -126,11 +164,9 @@ fn is_perfect_power(n: BigUint, k: BigUint, j: usize) -> bool {
 #[cfg(test)]
 mod tests {
     extern crate num;
-    use super::is_perfect_power;
-    use std::num::FromPrimitive;
-    use std::num::ToPrimitive;
+    use super::{bigint_exp, check_primality, is_perfect_power};
+    use std::num::{FromPrimitive, ToPrimitive};
     use num::bigint::BigUint;
-    use super::check_primality;
 
     #[test]
     fn test_perfect_power(){
@@ -143,4 +179,10 @@ mod tests {
         assert_eq!(check_primality(BigUint::from_u64(1024).unwrap(), 10), false);
     }
 
+    #[test]
+    fn test_bigint_ext(){
+        assert_eq!(bigint_exp(BigUint::from_usize(2).unwrap(), 
+                              BigUint::from_usize(8).unwrap()),
+                              BigUint::from_usize(256).unwrap());
+    }
 }
